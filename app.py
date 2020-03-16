9260d8da6ee0d6488220ffd1bb58907c5b19f3bd
obabichev <babichev.oleg.n@gmail.com>
2020-03-16 19:26:51 +0100

    hmmm....

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
 import os
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import Book
 
 app = Flask(__name__)
+
+print("os.environ['APP_SETTINGS']", os.environ['APP_SETTINGS'])
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+# from models import Book
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    author = db.Column(db.String())
+    published = db.Column(db.String())
+
+    def __init__(self, name, author, published):
+        self.name = name
+        self.author = author
+        self.published = published
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'author': self.author,
+            'published': self.published
+        }
 
 
 @app.route("/")
