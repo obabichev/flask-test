d887728b8843f7268ccb959761425c4d5b849288
obabichev <babichev.oleg.n@gmail.com>
2020-03-20 19:27:41 +0100

    remove book table

--- app.py
+++ app.py
@@ -12,36 +12,36 @@ db = SQLAlchemy(app)
 
 # from models import Book
 
-
-class Book(db.Model):
-    __tablename__ = 'books'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    author = db.Column(db.String())
-    published = db.Column(db.String())
-    neu = db.Column(db.String())
-    also = db.Column(db.String())
-
-    def __init__(self, name, author, published, neu, also):
-        self.name = name
-        self.author = author
-        self.published = published
-        self.neu = neu
-        self.also = also
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'author': self.author,
-            'published': self.published,
-            'neu': self.neu,
-            'also': self.also
-        }
+#
+# class Book(db.Model):
+#     __tablename__ = 'books'
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String())
+#     author = db.Column(db.String())
+#     published = db.Column(db.String())
+#     neu = db.Column(db.String())
+#     also = db.Column(db.String())
+#
+#     def __init__(self, name, author, published, neu, also):
+#         self.name = name
+#         self.author = author
+#         self.published = published
+#         self.neu = neu
+#         self.also = also
+#
+#     def __repr__(self):
+#         return '<id {}>'.format(self.id)
+#
+#     def serialize(self):
+#         return {
+#             'id': self.id,
+#             'name': self.name,
+#             'author': self.author,
+#             'published': self.published,
+#             'neu': self.neu,
+#             'also': self.also
+#         }
 
 
 user_environment = db.Table(
