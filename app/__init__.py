53940cf0c338146bfac38b0f61e861d5d37fa17c
obabichev <babichev.oleg.n@gmail.com>
2020-03-21 10:17:55 +0100

    users migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
 from app import routes
+
+from app.models import User
 
 # def create_app():
 #     app = Flask(__name__, instance_relative_config=True)
