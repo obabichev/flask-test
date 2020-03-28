8a0f61068e918fa17082a62bc980bb26ebb7b77c
obabichev <babichev.oleg.n@gmail.com>
2020-03-28 09:46:19 +0100

    small changes in configs

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login = LoginManager(app)
 login.login_view = 'login'
