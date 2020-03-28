19d4152a79ff3e680437606b891c9b3d98ab4223
obabichev <babichev.oleg.n@gmail.com>
2020-03-28 12:29:27 +0100

    fix edit of username, add debug mode

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import os
+import logging
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 
@@ -21,3 +23,20 @@ from app.models import User, Environment, Resource
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Environment': Environment, 'Resource': Resource}
+
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
--- config.py
+++ config.py
@@ -11,6 +11,13 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -23,7 +30,7 @@ class StagingConfig(Config):
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
-    DEBUG = True
+    FLASK_DEBUG = os.environ['FLASK_DEBUG']
 
 
 class TestingConfig(Config):
